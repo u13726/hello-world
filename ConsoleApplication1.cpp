d5cf7310786ffb7df0c546428bfc8ca7538e1eec
u13726 <Bboelaert@gmail.com>
2016-07-02 14:03:51 +0200

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,14 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	char d;
+	printf("Hello world\n");
+	scanf_s("%c",&d);
+	return 0;
+}
+
